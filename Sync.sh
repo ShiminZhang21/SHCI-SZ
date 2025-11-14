2775ce92a595307cc0fafd75ad883a5681be8cbf
ShiminZhang21 <szhan213@ucsc.edu>
2025-11-14 17:26:04 -0600

    clean file

--- Sync.sh
+++
@@ -1,5 +0,0 @@
-rsync -avz ./src/ Perl:/global/common/software/m4507/szhan213/IC_solver/shci_dev_2025July/src/
-rsync -avz ./tools/ Perl:/global/common/software/m4507/szhan213/IC_solver/shci_dev_2025July/tools/
-
-#Sync server to local
-#rsync -avz Perl:/global/common/software/m4507/szhan213/IC_solver/shci_dev_2025July/src/ ./src
